c54fdb541567fc64cfd02b6d723504b9212de481
mfelipperd <felipperabelodurans@gmail.com>
2025-04-03 12:22:58 -0300

    feat: change to use on vite

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.BASEUR,
+  baseURL: process.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
